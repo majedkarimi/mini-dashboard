db6cc14e7c0d0334f4dfd6fec37c36baefcaa1a2
majedkarimi <majed.karimi3@gmail.com>
2025-12-24 17:19:15 -0800

    improvement: change font size

--- src/features/dashboard/pages/DashboardPage.tsx
+++ src/features/dashboard/pages/DashboardPage.tsx
@@ -9,10 +9,12 @@ const DashboardPage = () => {
       {/*     Quick Access */}
       <div className="max-w-7xl mx-auto">
         <div className="mb-8">
-          <h1 className="text-4xl font-medium text-gray-700 mb-2">
+          <h1 className="text-3xl font-medium text-gray-700 mb-2">
             دسترسی سریع
           </h1>
-          <p className="text-gray-400">مدیریت سریع و آسان کسب‌وکار قصابی شما</p>
+          <p className="text-gray-400 text-sm">
+            مدیریت سریع و آسان کسب‌وکار قصابی شما
+          </p>
         </div>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 pb-5 ">
